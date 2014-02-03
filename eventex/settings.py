85353b163a0f72a252b1bea9ea8b0075ef2d90a4
rg3915 <rg3915@yahoo.com.br>
2014-02-03 00:37:11 -0200

    database local

--- eventex/settings.py
+++ eventex/settings.py
@@ -46,14 +46,14 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
     'default': {
-        'defaul': dj_database_url.config(
-            default='sqlite3:///' + BASE_DIR.child('db.sqlite3'))
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR.child('db.sqlite3'),
     }
+    # 'default': {
+    #     'defaul': dj_database_url.config(
+    #         default='sqlite3:///' + BASE_DIR.child('db.sqlite3'))
+    # }
 }
 
 # Internationalization

18648be54c0b23fd0fe0ea201967083d32c2b617
rg3915 <rg3915@yahoo.com.br>
2014-02-13 22:36:51 -0200

    debug=false

--- eventex/settings.py
+++ eventex/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition

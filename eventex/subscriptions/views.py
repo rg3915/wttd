f1e8f926c78524deea738d0d9d2fa50e94867ff0
rg3915 <rg3915@yahoo.com.br>
2014-02-15 00:48:22 -0200

    Fugindo das views macarrônicas

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -6,13 +6,19 @@ from eventex.subscriptions.models import Subscription
 
 def subscribe(request):
 	if request.method == 'POST':
-		form = SubscriptionForm(request.POST)
-		if form.is_valid():
-			obj = form.save()
-			return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
-		else:
-			return render(request, 'subscriptions/subscription_form.html',
-						  {'form': form})
+		return create(request)
 	else:
-		return render(request, 'subscriptions/subscription_form.html',
+		return new(request)
+
+def new(request):
+	return render(request, 'subscriptions/subscription_form.html',
 				  {'form': SubscriptionForm()})
+
+def create(request):
+	form = SubscriptionForm(request.POST)
+	if not form.is_valid():
+		return render(request, 'subscriptions/subscription_form.html',
+			          {'form': form})
+
+	obj = form.save()
+	return HttpResponseRedirect('/inscricao/%d/' % obj.pk)

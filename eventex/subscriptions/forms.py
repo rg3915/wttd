cb224f2873366448e011b41cbc267ff87ece79a3
rg3915 <rg3915@yahoo.com.br>
2014-02-14 22:09:36 -0200

    subscription form and tests

---
+++ eventex/subscriptions/forms.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from django import forms
+from django.utils.translation import ugettext as _
+
+class SubscriptionForm(forms.Form):
+	name = forms.CharField(label=_('Nome'))
+	cpf = forms.CharField(label=_('CPF'))
+	email = forms.EmailField(label=_('Email'))
+	phone = forms.CharField(label=_('Telefone'))
--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscribeTest(TestCase):
 	def setUp(self):
@@ -12,3 +13,25 @@ class SubscribeTest(TestCase):
 	def test_template(self):
 		'Response should be a rendered template.'
 		self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+
+	def test_html(self):
+		'Html must contain input controls.'
+		self.assertContains(self.resp, '<form')
+		self.assertContains(self.resp, '<input', 6)
+		self.assertContains(self.resp, 'type="text"', 3)
+		self.assertContains(self.resp, 'type="email"')
+		self.assertContains(self.resp, 'type="submit"')
+
+	def test_csrf(self):
+		'Html must contain csrf token.'
+		self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+	def test_has_form(self):
+		'Context must have the subscription form.'
+		form = self.resp.context['form']
+		self.assertIsInstance(form, SubscriptionForm)
+
+	def test_form_has_fields(self):
+		'Form must have 4 fields.'
+		form = self.resp.context['form']
+		self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)

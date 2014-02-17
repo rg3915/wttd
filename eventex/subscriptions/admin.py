772a64743bbb857a97b80dfccb7d6bfbf46a1152
rg3915 <rg3915@yahoo.com.br>
2014-02-16 23:18:29 -0300

    refatorando rotas

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from eventex.subscriptions.models import Subscription
 
-# Register your models here.
+admin.site.register(Subscription)
--- eventex/urls.py
+++ eventex/urls.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'eventex.core.views.home', name='home'),
-    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
-    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
-    url(r'^(.+)/$', 'eventex.core.views.template_view', name='tpl'),
+    url(r'', include('eventex.core.urls', namespace='core')),
+    url(r'^inscricao/$', include('eventex.subscriptions.urls', namespace='subscription')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(.+)/$', 'eventex.core.views.template_view', name='tpl'),
 )

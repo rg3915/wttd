229f4deb9b7d7dc98bc594494718561766a82225
rg3915 <rg3915@yahoo.com.br>
2014-02-16 23:32:07 -0300

    ajustando urls

--- eventex/core/urls.py
+++ eventex/core/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('eventex.core.views',
     url(r'^$', 'home', name='home'),
+    url(r'^(.+)/$', 'template_view', name='tpl'),
 )
--- eventex/urls.py
+++ eventex/urls.py
@@ -5,8 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include('eventex.core.urls', namespace='core')),
     url(r'^inscricao/$', include('eventex.subscriptions.urls', namespace='subscription')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^(.+)/$', 'eventex.core.views.template_view', name='tpl'),
+    url(r'', include('eventex.core.urls', namespace='core')),
 )

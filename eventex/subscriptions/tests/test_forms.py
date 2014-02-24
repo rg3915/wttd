9fdf9fc79da26f045a25a2d4abe0cd5f5bddf293
rg3915 <rg3915@yahoo.com.br>
2014-02-24 01:12:40 -0300

    test must inform email or phone

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -28,6 +28,11 @@ class SubscriptionFormTest(TestCase):
 		form = self.make_validated_form(name='REGIS DA SILVA')
 		self.assertEqual('Regis Da Silva', form.cleaned_data['name'])
 
+	def test_must_inform_email_or_phone(self):
+		'Email and Phone are optional, but one must be informed.'
+		form = self.make_validated_form(email='', phone='')
+		self.assertItemsEqual(['__all__'], form.errors)
+
 	def make_validated_form(self, **kwargs):
 		data = dict(
 			name='Regis da Silva',

8c6be5c960a19e49524f4c8c1f35a4cdfa208aa9
rg3915 <rg3915@yahoo.com.br>
2014-02-24 00:05:08 -0300

    CPFValidator

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -7,3 +7,17 @@ class SubscriptionFormTest(TestCase):
 		'Form must have 4 fields.'
 		form = SubscriptionForm()
 		self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)
+
+	def test_cpf_is_digit(self):
+		'CPF must only accept digits.'
+		data = dict(
+			name='Regis da Silva',
+			cpf='00000000000',
+			email='regis.santos.100@gmail.com',
+			phone='11-00000000'
+		)
+		data.update({'cpf': 'ABCD0000000'})
+		form = SubscriptionForm(data)
+		form.is_valid()
+
+		self.assertItemsEqual(['cpf'], form.errors)
